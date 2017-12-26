3a413a051590fc312ebef305f3c6382cae047234
Erick Chacon Montalvan <eral.th07@gmail.com>
2017-12-26 19:09:28 +0000

    remove fake gaussian process

--- R/effects-uni.R
+++ R/effects-uni.R
@@ -67,49 +67,8 @@ re <- function (x, sigma, groups, size = NULL) {
   return(output)
 }
 
-#' @title Gaussian process effect
-#'
-#' @description
-#' \code{function} description.
-#'
-#' @details
-#' details.
-#'
-#' @param par.
-#'
-#' @return return.
-#'
-#' @author Erick A. Chacon-Montalvan
-#'
-#' @examples
-#'
-#' (x <- gp(s1 = NA, s2 = NA, beta = -1:1, size = 10))
-#' (s1 <- x[[1]])
-#' (s2 <- x[[2]])
-#' gp(s1, s2, beta = -1:0)
-#'
-#' (s1 <- runif(10))
-#' (x <- gp(s1, s2 = NA, beta = -1:1, size = 10))
-#' (s2 <- x[[1]])
-#' gp(s1, s2, beta = -1:0)
-#'
-#' @export
-gp <- function (..., beta, size = NULL) {
-  vars <- list(...)
-  nvar <- purrr::map(vars, is.na) %>% do.call(sum, .)
-  if (!is.null(size)) {
-    output <- replicate(nvar, list(runif(size)))
-  } else {
-    output <- purrr::reduce(vars, `+`)
-  }
-  return(output)
-}
-
 exp_cor <- function (d, phi) {
   exp(-d/phi)
-}
-exp_cov <- function (d, phi, sigma2) {
-  sigma2 * exp(-d/phi)
 }
 
 #' @title Simulate a Gaussian process
